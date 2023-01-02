17c2d2551453eeab240394aff373c251326c7bf5
C. Augusto Proiete <augusto@proiete.com>
2023-01-02 12:45:08 -0300

    Update Copyright year to 2023

--- src/Cake.MinVer/MinVerVerbosity.cs
+++ src/Cake.MinVer/MinVerVerbosity.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright 2020-2022 C. Augusto Proiete & Contributors
+﻿#region Copyright 2020-2023 C. Augusto Proiete & Contributors
 //
 // Licensed under the MIT (the "License");
 // you may not use this file except in compliance with the License.
